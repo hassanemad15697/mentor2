fd1e4c6818324d86f702f88c4e80504e3a29509b
Hassan Askar <hassanemad15697@gmail.com>
2024-04-27 21:27:12 +0200

    init project

--- src/main/java/com/mentor2/mentor2/Mentor2Application.java
+++ src/main/java/com/mentor2/mentor2/Mentor2Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Mentor2Application {
 
+    // develop
     public static void main(String[] args) {
         SpringApplication.run(Mentor2Application.class, args);
     }
